ac750a188db77d056f31cb75666288f327ebdeb6
tsenger16 <eric.tseng3@gmail.com>
2016-12-06 09:14:56 -0500

    Update Arduino_101_BLE_EyeoT.ino

--- IoT/Arduino_101_BLE_EyeoT/Arduino_101_BLE_EyeoT.ino
+++ IoT/Arduino_101_BLE_EyeoT/Arduino_101_BLE_EyeoT.ino
@@ -1,10 +1,27 @@
- #include <Debug.h>        // contains print functions for verbose debugging.
+#include <Debug.h>        // contains print functions for verbose debugging.
 #include <EyeoT_BLE_Commands_and_Controls.h> // contains all EyeoT BLE commands and their actions
 #include <EyeoT_Servo.h>  // contains all EyeoT servo constants and control functions
 #include <EyeoT_Light_Strip.h> // contains functions and constants related to color and pin mapping of the indicator light strip    
+// #include <CurieTimerOne.h>
+
+//callback function used for timer delay
+/*void checkPhyiscalSwitchState(){
+  currentState.setValue(getState());
+}
+*/
 
 void setup() {
+
+
+  //Servo adjustments
+ //pinMode(9, INPUT);
+  
   Serial.begin(9600);
+
+  //Timer functions for delays
+
+  //Attach the interupt function to the callback function checkPhyiscalSwitchState()
+  //CurieTimerOne.attachInterrupt(checkPhyiscalSwitchState);
 
   // set the local name peripheral advertises
   blePeripheral.setLocalName("EyeoT_BLE");
@@ -37,6 +54,11 @@ void setup() {
   lightSwitch = getState();
   currentState.setValue(getState()); //needs testing
   currentResponse.setValue(READY);
+
+  //pinMode(9,OUTPUT);
+
+  // start timer interrupt; every 1 second it reads the current state of the device
+  // CurieTimerOne.start(1000000, checkPhyiscalSwitchState);
 }
 
 /*
@@ -49,7 +71,7 @@ void setup() {
 
 bool serviceCommmand(unsigned short command)
 {           
-   switch (command) 
+   switch (command)
    {
       currentResponse.setValue(NOT_READY); // for all cases, indicate that the Arduino is no longer ready to recieve further commands
       case NO_LIGHT: // turn off indicator lights
@@ -70,19 +92,32 @@ bool serviceCommmand(unsigned short command)
           }
           delay(250); // save power for the servo motor
           lightSwitch = Move(ON);
-          currentState.setValue(Move(ON));
+          currentState.setValue(DEVICE_ON);
           DEBUG_PRINT("turn light switch on ");
-          return (lightSwitch == ON) ? setLightColor(green): setLightColor(off);
+            if (lightSwitch == ON)
+          {
+            setLightColor(green);
+            delay(2000);
+            setLightColor(off);
+            return true;
+          }
+          return false;
       case SERVO_OFF: // move servo motor so light switch turns off
          if(!setLightColor(off)){
             DEBUG_PRINTLN("ERROR: Indicator lights not properly turned off!");
          }
          delay(250);
          lightSwitch = Move(OFF);
-         currentState.setValue(Move(OFF));
-         DEBUG_PRINT("turn light switch off ");
-         return (lightSwitch == OFF) ? setLightColor(red): setLightColor(off);
-  
+         currentState.setValue(DEVICE_OFF);
+         DEBUG_PRINT("turn light switch off "); 
+         if (lightSwitch == OFF)
+          {
+            setLightColor(red);
+            delay(2000);
+            setLightColor(off);
+            return true;
+          }
+          return false;
       default: 
          DEBUG_PRINTLN("ERROR: Unknown BLE Command Received!");
          return false;
@@ -91,18 +126,23 @@ bool serviceCommmand(unsigned short command)
 
 void loop(){
 
+// need to add a hardware interrupt that fires only when the switch is different
 // listen for BLE peripherals to connect:
   BLECentral central = blePeripheral.central();
 
   // if a central is connected to peripheral:
-  currentState.setValue(getState());
+  // currentState.setValue(getState());
   if (central) {
+    //now that a device is connected, pause the timer for state reads
+   // CurieTimerOne.pause();
+    
     DEBUG_PRINT("Connected to central: ");
     // print the central's MAC address:
     DEBUG_PRINTLN(central.address());
 
     // while the central is still connected to peripheral:
     while (central.connected()) {
+      
       // if the remote device writes a command, use it's value
       if (currentCommand.written()) {
         currentResponse.setValue(COMMAND_ACK); // indicate that the command has been received
@@ -112,22 +152,23 @@ void loop(){
         DEBUG_PRINT("Command ");       
         if(serviceCommmand(currentCommand.value() - 48)){
           currentResponse.setValue(COMMAND_SUCCESS); // indicate that the command has been received
-          currentState.setValue(getState());
-          DEBUG_PRINTLN("executed successfully!");      
+         //currentState.setValue(getState());
+          DEBUG_PRINTLN("executed successfully!");  
         }
         else{
           currentResponse.setValue(COMMAND_FAILURE); // indicate that the command failed
-          currentState.setValue(getState());
+         // currentState.setValue(getState());
           DEBUG_PRINTLN("ERROR: Command failed!");     
         }
         currentResponse.setValue(READY); // indicate that the Arduino is free to accept further commands
-        currentState.setValue(getState());
+       // currentState.setValue(getState());
         DEBUG_PRINTLN("Arduino ready for future command(s)!");       
       }
     }
 
     // when the central disconnects, print it out:
     DEBUG_PRINT(F("Disconnected from central: "));
+    //CurieTimerOne.resume(); //resume the timer once device is disconnected
     DEBUG_PRINTLN(central.address());
   }
 
